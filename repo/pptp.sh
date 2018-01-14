64474ef695e59da328ae78083814c740e4d34c4c
brantbell <31989675+brantbell@users.noreply.github.com>
2018-01-14 10:33:34 +0800

    Add files via upload

--- repo/pptp.sh
+++ repo/pptp.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
-cyan='\e[0;36m'
-green='\e[0;34m'
-okegreen='\033[92m'
-lightgreen='\e[1;32m'
-white='\e[1;37m'
-red='\e[1;31m'
-yellow='\e[1;33m'
-BlueF='\e[1;34m'
+
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+#MYIP=$(wget -qO- ipv4.icanhazip.com)
+
+#vps="zvur";
+vps="aneka";
+
+
+	source="https://raw.githubusercontent.com/elangoverdosis88/deenie"
+
 
 # go to root
 cd
 
+# check registered ip
+wget -q -O IP $source/debian7/IP.txt
+if ! grep -w -q $MYIP IP; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "zvur" ]]; then
+		echo "Hubungi: Yusuf ardiansyah atau elang overdosis)"
+	else
+		echo "Hubungi: Yusuf ardiansyah atau elang overdosis)"
+	fi
+	rm -f /root/IP
+	exit
+fi
+
+u="deenie11"
+p="deenie"
+
 # get the VPS IP
 #ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
 ip=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+if [ "$ip" = "" ]; then
+	ip=$(wget -qO- ipv4.icanhazip.com)
+fi
 
-
-echo ""
-echo -e $green""
+echo
 echo "######################################################"
-echo "    ----- Downloading and Installing PoPToP  ------"
+echo "Downloading and Installing PoPToP"
 echo "######################################################"
 apt-get update
 apt-get -y install pptpd
 
 echo
-echo -e $yellow""
 echo "######################################################"
-echo "      ----- Creating Server Config   -------          "
+echo "Creating Server Config"
 echo "######################################################"
 cat > /etc/ppp/pptpd-options <<END
 name pptpd
@@ -45,7 +67,7 @@ nobsdcomp
 END
 
 # setting up pptpd.conf
-echo -e $white"option /etc/ppp/pptpd-options" > /etc/pptpd.conf
+echo "option /etc/ppp/pptpd-options" > /etc/pptpd.conf
 echo "logwtmp" >> /etc/pptpd.conf
 echo "localip $ip" >> /etc/pptpd.conf
 echo "remoteip 10.1.0.1-100" >> /etc/pptpd.conf
@@ -54,7 +76,7 @@ echo "remoteip 10.1.0.1-100" >> /etc/pptpd.conf
 echo "$u	*	$p	*" >> /etc/ppp/chap-secrets
 
 echo
-echo -e $BlueF"######################################################"
+echo "######################################################"
 echo "Forwarding IPv4 and Enabling it on boot"
 echo "######################################################"
 cat >> /etc/sysctl.conf <<END
@@ -63,7 +85,7 @@ END
 sysctl -p
 
 echo
-echo -e $cyan"######################################################"
+echo "######################################################"
 echo "Updating IPtables Routing and Enabling it on boot"
 echo "######################################################"
 iptables -t nat -A POSTROUTING -j SNAT --to $ip
@@ -81,18 +103,18 @@ ifconfig ppp0 mtu 1400
 END
 
 echo
-echo -e $red""
 echo "######################################################"
-echo "        -------- Restarting PoPToP ------------       "
+echo "Restarting PoPToP"
 echo "######################################################"
 sleep 5
 /etc/init.d/pptpd restart
 
 echo
-echo -e $white"######################################################"
+echo "######################################################"
 echo "Server setup complete!"
 echo "Connect to your VPS at $ip with these credentials:"
 echo "Username:$u ##### Password: $p"
 echo "######################################################"
 
 cd ~/
+rm -f /root/IP
