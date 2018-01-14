81303c73d74907f10daf05b5879c478f44b9b639
brantbell <31989675+brantbell@users.noreply.github.com>
2018-01-14 11:26:34 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -21,24 +21,24 @@ cd
 
 echo -e $yellow "[ ! ]  Proses Updating Script VPS .....  "
 echo -e $yellow "[ ! ]  ................     Please wait  "
-wget -O /usr/bin/dropmon "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/dropmon.sh" >> $log 2>&1
-wget -O /usr/bin/menu "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/menu.sh" >> $log 2>&1
-wget -O /usr/bin/user-add "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-add.sh" >> $log 2>&1
-wget -O /usr/bin/user-add-vpn "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-add-vpn.sh" >> $log 2>&1
-wget -O /usr/bin/user-add-pptp "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-add-pptp.sh" >> $log 2>&1
-wget -O /usr/bin/user-expire "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-expire.sh" >> $log 2>&1
-wget -O /usr/bin/user-gen "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-gen.sh" >> $log 2>&1
-wget -O /usr/bin/user-limit "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-limit.sh" >> $log 2>&1
-wget -O /usr/bin/user-list "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-list.sh" >> $log 2>&1
-wget -O /usr/bin/user-login "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-login.sh" >> $log 2>&1
-wget -O /usr/bin/user-active-list "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-active-list.sh" >> $log 2>&1
-wget -O /usr/bin/user-renew "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-renew.sh" >> $log 2>&1
-wget -O /usr/bin/user-del "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-del.sh" >> $log 2>&1
-wget -O /usr/bin/user-pass "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-pass.sh" >> $log 2>&1
-wget -O /usr/bin/user-expire-list "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-expire-list.sh" >> $log 2>&1
-wget -O /usr/bin/user-banned "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/user-banned.sh" >> $log 2>&1
-wget -O /usr/bin/unbanned-user "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/unbanned-user.sh" >> $log 2>&1
-wget -O /usr/bin/delete-user-expire "https://raw.githubusercontent.com/Mbah-Shondong/Debian732/master/Debian7/delete-user-expire.sh" >> $log 2>&1
+wget -O /usr/bin/dropmon "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/dropmon.sh" >> $log 2>&1
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/menu.sh" >> $log 2>&1
+wget -O /usr/bin/user-add "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-add.sh" >> $log 2>&1
+wget -O /usr/bin/user-add-vpn "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-add-vpn.sh" >> $log 2>&1
+wget -O /usr/bin/user-add-pptp "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-add-pptp.sh" >> $log 2>&1
+wget -O /usr/bin/user-expire "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-expire.sh" >> $log 2>&1
+wget -O /usr/bin/user-gen "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-gen.sh" >> $log 2>&1
+wget -O /usr/bin/user-limit "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-limit.sh" >> $log 2>&1
+wget -O /usr/bin/user-list "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-list.sh" >> $log 2>&1
+wget -O /usr/bin/user-login "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-login.sh" >> $log 2>&1
+wget -O /usr/bin/user-active-list "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-active-list.sh" >> $log 2>&1
+wget -O /usr/bin/user-renew "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-renew.sh" >> $log 2>&1
+wget -O /usr/bin/user-del "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-del.sh" >> $log 2>&1
+wget -O /usr/bin/user-pass "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-pass.sh" >> $log 2>&1
+wget -O /usr/bin/user-expire-list "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-expire-list.sh" >> $log 2>&1
+wget -O /usr/bin/user-banned "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/user-banned.sh" >> $log 2>&1
+wget -O /usr/bin/unbanned-user "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/unbanned-user.sh" >> $log 2>&1
+wget -O /usr/bin/delete-user-expire "https://raw.githubusercontent.com/brantbell/wulandari/srie/repo/delete-user-expire.sh" >> $log 2>&1
 
 chmod +x /usr/bin/dropmon >> $log 2>&1
 chmod +x /usr/bin/menu >> $log 2>&1
@@ -59,6 +59,6 @@ chmod +x /usr/bin/user-banned >> $log 2>&1
 chmod +x /usr/bin/unbanned-user >> $log 2>&1
 chmod +x /usr/bin/delete-user-expire >> $log 2>&1
 echo -e $lightgreen "[ ✔ ]  Script Now Updated     "
-echo -e $white "Terimakasih SUdah Menggunakan Script ./MBAH SHONDONG"
+echo -e $white "Terimakasih SUdah Menggunakan Script ./ZHANGZI-MANIA"
 rm update.sh
 history -c
