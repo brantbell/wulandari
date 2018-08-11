7da39ec008b537688c258e12ee7546c345d7c953
brantbell <31989675+brantbell@users.noreply.github.com>
2018-08-11 08:07:27 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-if [ $USER != 'root' ]; then
-	echo "Maaf, Anda harus menjalankan ini sebagai root"
-	exit
+#if [ $USER != 'root' ]; then
+#	echo "Maaf, Anda harus menjalankan ini sebagai root"
+#	exit
+#fi
+#Requirement
+if [ ! -e /usr/bin/curl ]; then
+    apt-get -y update && apt-get -y upgrade
+	apt-get -y install curl
 fi
-
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
